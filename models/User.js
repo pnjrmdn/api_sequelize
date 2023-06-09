3895875c67bf5b9a4fed1f5bc8e6c27da566c86d
Panji Ramadanu <36503158+pnjrmdn@users.noreply.github.com>
2023-06-09 11:08:45 +0800

    initial commit

--- models/User.js
+++ models/User.js
@@ -18,12 +18,14 @@ module.exports = (sequelize, DataTypes) => {
             type: DataTypes.STRING,
             allowNull: false,
            },
-           createdAt: {
+           createdAt: 
+           {
             type: DataTypes.DATE,
             allowNull: false,
             field: "created_at",
            },
-           updatedAt: {
+           updatedAt: 
+           {
             type: DataTypes.DATE,
             allowNull: false,
             field: "updated_at",
